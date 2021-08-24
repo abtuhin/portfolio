304c9447ba2672dee87cb0ab38270b8eca194793
tuhin <tuhin9pro@gmail.com>
2021-08-24 04:00:42 +0200

    Updates

---
+++ precache-manifest.dbc1c25066ce3f735be643f705414802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9936da0edf06ca77bba9cbd8f0c1a052",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2be8cd2ef8db371d6368",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0ddf7c74e1832075177d",
+    "url": "./static/css/main.30c92c89.chunk.css"
+  },
+  {
+    "revision": "2be8cd2ef8db371d6368",
+    "url": "./static/js/2.d0c008ca.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.d0c008ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ddf7c74e1832075177d",
+    "url": "./static/js/main.d2e056f6.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
